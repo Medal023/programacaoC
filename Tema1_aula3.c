94a3f0b3cd12b5c66152b288921f4a3af5dcd9d9
Medal023 <alisonvitoriomedal@gmail.com>
2025-03-27 17:02:15 -0300

    Update and rename EXERCICIOS FACULDADE EM .C to Tema1_aula3.c

---
+++ Tema1_aula3.c
@@ -0,0 +1,9 @@
+#include    <stdio.h>
+
+int main(){
+    //printf() envia informações ao monitor.
+    //\n Adiciona uma nova linha ao final da mensagem.
+    printf("Boa Tarde!\n");
+    //return 0; indica que esse programa terminou com sucesso.
+    return 0;
+}
