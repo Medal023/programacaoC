b8e81733cc773aa46f349ff03f792632dec2fe27
Medal023 <alisonvitoriomedal@gmail.com>
2025-03-27 23:24:09 -0300

    Update N1_CN4.c

--- N1_CN4.c
+++ N1_CN4.c
@@ -1,8 +1,16 @@
-#include  <stdio.h>
+#include <stdio.h>
 
 int main(){
 
-  int idade
-  int
-  
+    //int é um tipo de dados inteiro, ele diz que sera armazenado um dados inteiro.
+    int idade = 25;
+    int quantidade = 1;
+    //float é um ponto flutuante, trabalha com casas decimais por exemplo: float altura;(1.75) ou seja, ele armazena um numero decimal.
+    float altura = 1.75;
+    //double tambem armazena variaveis de ponto flutuante / valores decimais, so que em maiores valores.
+    double peso = 90.3;
+    //char armazena caracteres, por exemplo uma letra, caso queira armazenar mais de um caracter e necessario usar [], por exemplo: char nome[20].
+    char letra = 'A';
+    char nome[20] = "Alison";
+
 }
